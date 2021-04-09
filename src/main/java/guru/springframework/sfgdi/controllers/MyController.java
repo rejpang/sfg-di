e61101e8cb9822eaaa302b12394e7fde52ff8591
rex <rex.panganiban@webbfontaine.com>
2021-04-09 15:29:28 +0800

    Section 3 - Primary Beans

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,12 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World !!");
+
         return "Hi Folks!";
     }
 
